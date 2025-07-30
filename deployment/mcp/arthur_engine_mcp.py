d179828899817ee932ce02ee571fb1bd19a738ff
Tal Erez <tal@arthur.ai>
2025-07-29 23:37:25 -0400

    add validation endpoints to mcp

--- deployment/mcp/arthur_engine_mcp.py
+++ deployment/mcp/arthur_engine_mcp.py
@@ -1,12 +1,14 @@
-from fastmcp import FastMCP
+import os
+
 import requests
 from dotenv import load_dotenv
-import os
+from fastmcp import FastMCP
 
 load_dotenv()
 
-GENAI_ENGINE_API_KEY = os.getenv('GENAI_ENGINE_API_KEY')
-GENAI_ENGINE_BASEURL = os.getenv('GENAI_ENGINE_BASEURL') + "/api/v2"
+GENAI_ENGINE_API_KEY = os.getenv("GENAI_ENGINE_API_KEY")
+GENAI_ENGINE_BASEURL = os.getenv("GENAI_ENGINE_BASEURL") + "/api/v2"
+GENAI_ENGINE_TASK_ID = os.getenv("GENAI_ENGINE_TASK_ID")
 
 mcp = FastMCP(
     name="arthur-engine-mcp",
@@ -16,16 +18,98 @@ mcp = FastMCP(
     """,
 )
 
+
+def validate_prompt(prompt, task_id):
+    validate_prompt_url = f"{GENAI_ENGINE_BASEURL}/tasks/{task_id}/validate_prompt"
+    return requests.post(
+        validate_prompt_url,
+        json={"prompt": prompt, "conversation_id": "1", "user_id": "1"},
+        headers={"Authorization": f"Bearer {GENAI_ENGINE_API_KEY}"},
+    )
+
+
+def validate_response(llm_response, context, task_id):
+    validate_prompt_response = validate_prompt(context, task_id)
+
+    if validate_prompt_response.status_code != 200:
+        return "Failed to validate prompt"
+
+    validate_prompt_response_json = validate_prompt_response.json()
+    inference_id = validate_prompt_response_json["inference_id"]
+    validate_response_url = (
+        f"{GENAI_ENGINE_BASEURL}/tasks/{task_id}/validate_response/{inference_id}"
+    )
+    return requests.post(
+        validate_response_url,
+        json={"response": llm_response, "context": context},
+        headers={"Authorization": f"Bearer {GENAI_ENGINE_API_KEY}"},
+    )
+
+
+@mcp.tool
+def run_guardrails_on_prompt(user_input_text: str):
+    """
+    Checks various guardrails over a user's input. This should be called before generating any response for the user.
+    If this check fails, you should not respond to a user's request and you should only respond with the returned
+    response text of this function.
+
+    Parameters:
+        user_input_text (str) - The user's input text.
+    """
+    response = validate_prompt(user_input_text, GENAI_ENGINE_TASK_ID)
+    if response.status_code != 200:
+        return f"Failed to validate prompt: {response.status_code} {response.text}"
+
+    response_json = response.json()
+
+    if "rule_results" not in response_json:
+        return f"Failed to validate prompt: {response.status_code} {response.text}"
+
+    rule_results = response_json["rule_results"]
+    for rule_result in rule_results:
+        if rule_result["result"] != "Pass":
+            return f"{rule_result['rule_type']} Failed"
+
+    return f"All rules passed!"
+
+
+@mcp.tool
+def run_guardrails_on_response(llm_response_text: str):
+    """
+    Checks various guardrails over an LLM response. This should be called by giving your generated response to the
+    user as a parameter.If this check fails, you should not respond to a user's request and you should only respond with the returned
+    response text of this function.
+
+    Parameters:
+        llm_response_text (str) - The generated response from the LLM.
+    """
+    response = validate_response(llm_response_text, "", GENAI_ENGINE_TASK_ID)
+    if response.status_code == 200:
+        response_json = response.json()
+
+        if "rule_results" not in response_json:
+            return (
+                f"Failed to validate response: {response.status_code} {response.text}"
+            )
+
+        rule_results = response_json["rule_results"]
+        for rule_result in rule_results:
+            if rule_result["result"] != "Pass":
+                return f"{rule_result['rule_type']} Failed"
+
+        return f"All rules passed!"
+
+
 @mcp.tool
 def create_toxicity_task(
-    task_name: str = "ToxicityTask", 
-    rule_name: str = "Toxicity Rule", 
-    apply_to_prompt: bool = True, 
-    apply_to_response: bool = True, 
+    task_name: str = "ToxicityTask",
+    rule_name: str = "Toxicity Rule",
+    apply_to_prompt: bool = True,
+    apply_to_response: bool = True,
     toxicity_threshold: float = 0.5,
 ):
     """
-    Creates a task in the Arthur Engine which has a toxicity rule applied to it. 
+    Creates a task in the Arthur Engine which has a toxicity rule applied to it.
     All parameters are optional and have default values.
 
     Parameters:
@@ -40,36 +124,37 @@ def create_toxicity_task(
     """
     task_url = GENAI_ENGINE_BASEURL + "/task"
     task_response = requests.post(
-        task_url, 
-        json={"name": task_name}, 
-        headers={"Authorization": f"Bearer {GENAI_ENGINE_API_KEY}"}
+        task_url,
+        json={"name": task_name},
+        headers={"Authorization": f"Bearer {GENAI_ENGINE_API_KEY}"},
     )
 
     if task_response.status_code == 200:
         task_response_json = task_response.json()
-        task_id = task_response_json['id']
+        task_id = task_response_json["id"]
 
         rule_url = f"{GENAI_ENGINE_BASEURL}/tasks/{task_id}/rules"
         rule_response = requests.post(
-            rule_url, 
+            rule_url,
             json={
-                "name": rule_name, 
-                "type": "ToxicityRule", 
-                "apply_to_prompt": apply_to_prompt, 
+                "name": rule_name,
+                "type": "ToxicityRule",
+                "apply_to_prompt": apply_to_prompt,
                 "apply_to_response": apply_to_response,
                 "config": {"threshold": toxicity_threshold},
-            }, 
-            headers={"Authorization": f"Bearer {GENAI_ENGINE_API_KEY}"}
+            },
+            headers={"Authorization": f"Bearer {GENAI_ENGINE_API_KEY}"},
         )
 
         if rule_response.status_code == 200:
             rule_response_json = rule_response.json()
-            rule_id = rule_response_json['id']
+            rule_id = rule_response_json["id"]
             return f"Toxicity task created with ID: {task_id} and rule ID: {rule_id}"
         else:
             return f"Failed to create rule: {rule_response.status_code} {rule_response.text}"
-    
+
     return f"Failed to create task: {task_response.status_code} {task_response.text}"
+
 
 if __name__ == "__main__":
     mcp.run()
