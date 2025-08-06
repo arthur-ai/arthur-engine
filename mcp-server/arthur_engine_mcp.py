95fc20a59f94c568d9b0d3ab5a01fa41d4b58c20
Tal Erez <tal@arthur.ai>
2025-08-06 13:53:25 -0400

    create separate admin mcp for arthur

--- mcp-server/arthur_engine_mcp.py
+++ mcp-server/arthur_engine_mcp.py
@@ -14,10 +14,7 @@ GENAI_ENGINE_TASK_ID = os.getenv("GENAI_ENGINE_TASK_ID")
 
 mcp = FastMCP(
     name="arthur-engine-mcp",
-    instructions="""
-        This server provides functionality to interact with the Arthur Engine API, a platform that provides LLM guardrails.
-        To setup guardrails we create tasks that have subsequent rules (e.g. toxicity, hallucination, etc.)
-    """,
+    instructions="""This server provides functionality to use the guardrails created and monitored by Arthur"""
 )
 
 class RuleTypesEnum(Enum):
@@ -153,114 +150,6 @@ def run_guardrails_on_response(llm_response_text: str, context: str, rule_types:
         return f"All rules passed!"
 
     return f"Failed rules: {', '.join(failed_rules)}"
-
-
-@mcp.tool
-def create_toxicity_task(
-    task_name: str = "ToxicityTask",
-    rule_name: str = "Toxicity Rule",
-    apply_to_prompt: bool = True,
-    apply_to_response: bool = True,
-    toxicity_threshold: float = 0.5,
-):
-    """
-    Creates a task in the Arthur Engine which has a toxicity rule applied to it.
-    All parameters are optional and have default values.
-
-    Parameters:
-        task_name (str) - The name of the task to create.
-        rule_name (str) - The name of the rule to create.
-        apply_to_prompt (bool) - Whether to apply the rule to the prompt.
-        apply_to_response (bool) - Whether to apply the rule to the response.
-        toxicity_threshold (float) - The threshold for the toxicity rule.
-
-    Returns:
-        str - The ID of the task and rule created and the status of the request.
-    """
-    task_url = GENAI_ENGINE_BASEURL + "/task"
-    task_response = requests.post(
-        task_url,
-        json={"name": task_name},
-        headers={"Authorization": f"Bearer {GENAI_ENGINE_API_KEY}"},
-    )
-
-    if task_response.status_code == 200:
-        task_response_json = task_response.json()
-        task_id = task_response_json["id"]
-
-        rule_url = f"{GENAI_ENGINE_BASEURL}/tasks/{task_id}/rules"
-        rule_response = requests.post(
-            rule_url,
-            json={
-                "name": rule_name,
-                "type": RuleTypesEnum.TOXICITY.value,
-                "apply_to_prompt": apply_to_prompt,
-                "apply_to_response": apply_to_response,
-                "config": {"threshold": toxicity_threshold},
-            },
-            headers={"Authorization": f"Bearer {GENAI_ENGINE_API_KEY}"},
-        )
-
-        if rule_response.status_code == 200:
-            rule_response_json = rule_response.json()
-            rule_id = rule_response_json["id"]
-            return f"Toxicity task created with ID: {task_id} and rule ID: {rule_id}"
-        else:
-            return f"Failed to create rule: {rule_response.status_code} {rule_response.text}"
-
-    return f"Failed to create task: {task_response.status_code} {task_response.text}"
-
-
-@mcp.tool
-def add_prompt_injection_rule_to_task(
-    task_name: str,
-    rule_name: str = "Prompt Injection Rule",
-):
-    """
-    Adds a prompt injection rule for an existing task in the Arthur Engine.
-
-    Parameters:
-        rule_name (str) - The name of the rule to create.
-        task_name (str) - The name of the task to add the rule to.
-
-    Returns:
-        str - The ID of the task and rule created and the status of the request.
-    """
-    task_search_url = f"{GENAI_ENGINE_BASEURL}/tasks/search?sort=desc&page_size=10&page=0"
-    task_response = requests.post(
-        task_search_url,
-        json={"name": task_name},
-        headers={"Authorization": f"Bearer {GENAI_ENGINE_API_KEY}"},
-    )
-
-    if task_response.status_code != 200:
-        return f"Failed to find task: {task_response.status_code} {task_response.text}"
-
-    task_response_json = task_response.json()
-    tasks = task_response_json["tasks"]
-
-    if len(tasks) == 0:
-        return f"No task found with name: {task_name}"
-
-    task_id = tasks[0]["id"]
-    rule_url = f"{GENAI_ENGINE_BASEURL}/tasks/{task_id}/rules"
-    rule_response = requests.post(
-        rule_url,
-        json={
-            "name": rule_name,
-            "type": RuleTypesEnum.PROMPT_INJECTION.value,
-            "apply_to_prompt": True,
-            "apply_to_response": False,
-        },
-        headers={"Authorization": f"Bearer {GENAI_ENGINE_API_KEY}"},
-    )
-
-    if rule_response.status_code == 200:
-        rule_response_json = rule_response.json()
-        rule_id = rule_response_json["id"]
-        return f"Successfully added prompt injection rule to task: {task_id} with rule ID: {rule_id}"
-    
-    return f"Failed to create rule: {rule_response.status_code} {rule_response.text}"
 
 if __name__ == "__main__":
     mcp.run()
