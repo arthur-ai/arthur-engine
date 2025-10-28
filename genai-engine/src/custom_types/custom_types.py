7241efdc9d3623b5d59472646766f3cd982367ca
Kacper Krupinski <kacper.krupinski@arthur.ai>
2025-10-28 16:32:22 +0100

    Fixup mypy issues for routers/v1 directory

--- genai-engine/src/custom_types/custom_types.py
+++ genai-engine/src/custom_types/custom_types.py
@@ -1,5 +1,6 @@
-from typing import TypeVar
+from typing import Any, Callable, TypeVar
 
 from sqlalchemy.orm import Query
 
 QueryT = TypeVar("QueryT", bound=Query)
+FunctionT = TypeVar("FunctionT", bound=Callable[..., Any])
