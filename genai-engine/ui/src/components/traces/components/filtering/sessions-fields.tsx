3d44a98f67dcd74897a1a5e8d7769ec63b26ed1e
Łukasz Myśliwiec <lukaszmysliwiec@blazity.com>
2025-12-04 12:57:28 +0100

    feat: change id fields to primitive text

--- genai-engine/ui/src/components/traces/components/filtering/sessions-fields.tsx
+++ genai-engine/ui/src/components/traces/components/filtering/sessions-fields.tsx
@@ -1,41 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
-
-import { useFilterStore } from "../../stores/filter.store";
-
-import { createDynamicEnumField, Field } from "./fields";
-import { EnumOperators } from "./types";
-
-import { Api } from "@/lib/api";
-import { MAX_PAGE_SIZE } from "@/lib/constants";
-import { queryKeys } from "@/lib/queryKeys";
-import { getUsers } from "@/services/tracing";
+import { createPrimitiveField, Field } from "./fields";
+import { Operators } from "./types";
 
 export const SESSION_FIELDS = [
-  createDynamicEnumField<{ taskId: string; api: Api<unknown> }, "user_ids">({
-    type: "dynamic_enum",
+  createPrimitiveField({
     name: "user_ids",
-    getTriggerClassName: () => "",
-    renderValue: (value) => [value].flat().join(", "),
-    operators: [EnumOperators.EQUALS, EnumOperators.IN],
-    itemToStringLabel: undefined,
-    useData: function useData({ taskId, api }) {
-      const timeRange = useFilterStore((state) => state.timeRange);
-
-      const params = {
-        taskId,
-        page: 0,
-        pageSize: MAX_PAGE_SIZE,
-        filters: [],
-        timeRange,
-      };
-
-      const { data, isLoading } = useQuery({
-        queryKey: queryKeys.users.listPaginated(params),
-        queryFn: () => getUsers(api, params),
-        select: (data) => data.users.map((user) => user.user_id),
-      });
-
-      return { data: data ?? [], loading: isLoading };
-    },
+    type: "text",
+    operators: [Operators.EQUALS],
   }),
 ] as const satisfies Field[];
