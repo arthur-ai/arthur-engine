6bb56273bde15104c8ecd5a5f4682b9ae6c2bd2b
Artur Sidwa <44118960+xV3rt@users.noreply.github.com>
2026-03-04 12:53:20 +0100

    UP-3833 Allow submission with no description of CEs (#1376)

--- genai-engine/ui/src/components/live-evals/new/index.tsx
+++ genai-engine/ui/src/components/live-evals/new/index.tsx
@@ -90,7 +90,7 @@ export const LiveEvalsNew = () => {
     onSubmit: async ({ value }) => {
       const { id } = await createContinuousEval.mutateAsync({
         name: value.name,
-        description: value.description,
+        description: value.description?.trim() || undefined,
         enabled: value.enabled,
         llm_eval_name: value.evaluator.name!,
         llm_eval_version: value.evaluator.version!,
