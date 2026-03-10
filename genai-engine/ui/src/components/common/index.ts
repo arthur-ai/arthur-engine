a688c35267079f42e88f2e4cab6b70a97260c29a
Łukasz Myśliwiec <lukaszmysliwiec@blazity.com>
2026-03-10 13:27:21 +0100

    feat(UP-3985): bump shared components (#1426)

--- genai-engine/ui/src/components/common/index.ts
+++ genai-engine/ui/src/components/common/index.ts
@@ -1,4 +1,3 @@
 export { CopyableChip } from "./CopyableChip";
-export { ErrorFallback } from "./ErrorFallback";
 export { ResultChip } from "./ResultChip";
 export { UpdateDatasetRowModal } from "./UpdateDatasetRowModal";
