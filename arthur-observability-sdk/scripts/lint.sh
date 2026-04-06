b1cd770c3695ea77ea94825bb657e8c8cc347b9a
ntatsumi <1480268+ntatsumi@users.noreply.github.com>
2026-04-06 15:26:50 -0400

    Manage working directly correctly

--- arthur-observability-sdk/scripts/lint.sh
+++ arthur-observability-sdk/scripts/lint.sh
@@ -4,11 +4,11 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-uv sync --project python --group dev -q
-uv run --project python black src/arthur_observability_sdk tests
-uv run --project python autoflake --remove-all-unused-imports --in-place --recursive --quiet src/arthur_observability_sdk tests
-uv run --project python isort src/arthur_observability_sdk tests --profile black
-uv run --project python mypy src/arthur_observability_sdk
+uv sync --directory python --group dev -q
+uv run --directory python black src/arthur_observability_sdk tests
+uv run --directory python autoflake --remove-all-unused-imports --in-place --recursive --quiet src/arthur_observability_sdk tests
+uv run --directory python isort src/arthur_observability_sdk tests --profile black
+uv run --directory python mypy src/arthur_observability_sdk
 
 YELLOW='\033[1;33m'
 NC='\033[0m'
