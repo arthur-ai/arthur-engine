1091d018b23f9cb4d9153780f56ebd69ec672d77
Martin Buitrago <martin@arthur.ai>
2026-05-28 14:28:06 -0400

    targeted style

--- genai-engine/ui/src/theme/mui-theme.ts
+++ genai-engine/ui/src/theme/mui-theme.ts
@@ -29,6 +29,17 @@ const createAppTheme = (mode: "light" | "dark") =>
           }),
         },
       },
+      MuiButton: {
+        styleOverrides: {
+          root: ({ theme }) => ({
+            ...(theme.palette.mode === "dark" && {
+              "&.Mui-disabled": {
+                color: theme.palette.text.disabled,
+              },
+            }),
+          }),
+        },
+      },
     },
   });
 
